8ddc190117fc84b4f8294ddb0329eb27dff9962d
CosimoTheMedici <cosmasthuku4@gmail.com>
2021-07-19 20:31:27 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import axios from 'axios';
+
+
+const App = () => {
+  return (
+    <div className = 'container'>
+      <h1>my application</h1>
+    </div>
+  )
+}
+
+export default App
